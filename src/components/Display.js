2897e8391dfb4b493683a47aef3ffdb978a31c46
Mason U. Borchard <mborchard@k12.com>
2022-03-13 17:48:40 -0700

    feat: added logic to render qbyte clock based on donation amount

--- src/components/Display.js
+++ src/components/Display.js
@@ -7,32 +7,45 @@ import Clock from './clock';
 //import Clock from 'react-clock';
 
 export default function Display({ currentUser, contract }) {
+  // const amt_donated = contract.get_donation({
+  //   account_id: currentUser.contractId,
+  // });
+  // var pretendDonated = 25;
 
+  var pretendDonated = 2;
+  var displayClock = false;
+  var hideDisplay = {
+    display: "none",
+  };
 
+  if (pretendDonated > 3) {
+    displayClock = true;
+    var showDisplay = {
+      display: "contents",
+    };
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-
-        <script>
-
-          var amt_donated = contract.get_donation({
-          account_id: currentUser.accountId
-          });
-
-          var pretend_donated = 5
-
-          console.log(pretend_donated)
-        </script>
-    
         <p>
-          <font size = "6">Hybrid-Quantum Computing Spatial Relativity Time Dilation Clock with Non-deterministic Machine Learning Language Oracle </font>
+          <font size="6">
+            Hybrid-Quantum Computing Spatial Relativity Time Dilation Clock with
+            Non-deterministic Machine Learning Language Oracle{" "}
+          </font>
         </p>
         <p>
-          <font size = "5">Welcome to the Q-Byte Clock, { currentUser.accountId }! You have donated YACHTO.</font>
+          <font size="5">
+            Welcome to the Q-Byte Clock, {currentUser.accountId}! You have
+            donated YACHTO.
+          </font>
         </p>
         <p>
-          <font size = "5">The Q-Byte Clock tells the time while demonstrating what time is. The full user experience is coming soon, meanwhile, you can access interactive graphs and visuals from our Github.</font>
+          <font size="5">
+            The Q-Byte Clock tells the time while demonstrating what time is.
+            The full user experience is coming soon, meanwhile, you can access
+            interactive graphs and visuals from our Github.
+          </font>
         </p>
         <a
           className="App-link"
@@ -41,7 +54,6 @@ export default function Display({ currentUser, contract }) {
           rel="noopener noreferrer"
         >
           Visit haloai.me Q-Byte Page
-          
         </a>
         <a
           className="App-link"
@@ -50,7 +62,6 @@ export default function Display({ currentUser, contract }) {
           rel="noopener noreferrer"
         >
           Github Repository for QByte Clock
-          
         </a>
         <a
           className="App-link"
@@ -59,21 +70,14 @@ export default function Display({ currentUser, contract }) {
           rel="noopener noreferrer"
         >
           Video Explanation for QByte Clock Data
-          
         </a>
 
-        {/* <script>
-        if (pretend_donated > 3){
-
-
-        }
-
-        </script> */}
-
-        <StyledEngineProvider injectFirst>
-        <Clock />
-          <RandomBytes />
-        </StyledEngineProvider>
+        <div style={displayClock ? showDisplay : hideDisplay}>
+          <StyledEngineProvider injectFirst>
+            <Clock />
+            <RandomBytes />
+          </StyledEngineProvider>
+        </div>
       </header>
 
       <img src={logo} className="App-logo" alt="logo" />
